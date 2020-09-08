751a143daa837330ece76c92dd5c5c4fffa984bc
unknown <theankitbagchi@gmail.com>
2020-09-08 18:45:18 -0400

    Merge conflicts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,7 @@ import React from "react";
 import Card from "../components/Card";
 
 function Home() {
-  return (
-    <h1>Hello</h1>
-    <h1>Hello</h1>
-  )
+  return <h1>Hello</h1>;
 }
 
 export default Home;
