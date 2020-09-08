d3d1988810f62a317d7a1f8ec876d7fbdb60054f
Joshua Wilensky <joshwilensky@gmail.com>
2020-09-08 18:53:19 -0400

    Added Jumbotron to the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
                   <p className="mx-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
                   optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
                 </p>
-                  <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View project</MDBBtn>
+                  <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View Project</MDBBtn>
                 </MDBCol>
               </MDBCol>
             </MDBJumbotron>
