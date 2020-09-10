7cd530bc1c37dcbf6cc6b3161ccc1ebf5d931f80
Melissa Mercado <melissa.a.merc@gmail.com>
2020-09-09 20:09:23 -0400

    routes

--- client/src/pages/Members/index.js
+++ client/src/pages/Members/index.js
@@ -3,7 +3,7 @@ import Birthdaycard from "../../components/Birthday";
 import Halloweencard from "../../components/Halloween";
 import Hannukahcard from "../../components/Hannukah";
 import Christmascard from "../../components/Christmas";
-
+import CardExample from "../../components/Card";
 /**
  *
  * @param {any[]} arr Array to select random item from
@@ -36,7 +36,11 @@ function Members() {
     });
 
   return (
-    <>{state.selected && <Card card={state.selected} onClick={getRandom} />}</>
+    <>
+      {state.selected && (
+        <CardExample card={state.selected} onClick={getRandom} />
+      )}
+    </>
   );
   // return (
   //   // <div>
