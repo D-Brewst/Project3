e2076c29daa93f05219b828dc1b9f9c4e99599e7
D-Brewst <devonbrewster31@gmail.com>
2020-09-14 13:15:23 -0400

    changed color of occasion buttons and created options for pdf size

--- client/src/components/CardButton/index.js
+++ client/src/components/CardButton/index.js
@@ -2,12 +2,28 @@ import React from "react";
 import { MDBBtn } from "mdbreact";
 
 const CardButton = (props) => {
+  const Occasions = {
+    Birthday: {
+      color: "primary",
+    },
+    Christmas: {
+      color: "success",
+    },
+    Valentine: {
+      color: "danger",
+    },
+    Hanukkah: {
+      color: "info",
+    },
+  };
+
   return (
     <MDBBtn
       onClick={() => props.genRandom(props.text)}
       className={`card-btn ${props["data-value"]}`}
       gradient="red"
-    >
+      color= {Occasions[props.text] ? Occasions[props.text].color : "tree"}
+      >
       {props.text}
     </MDBBtn>
   );
